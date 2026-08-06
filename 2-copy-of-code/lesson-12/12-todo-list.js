bc274761d6cac1a6352b39f9a1caa311f03d33e6
John <jetjet9992002@gmail.com>
2026-08-07 00:51:22 +0800

    Lesson 12 part 1

---
+++ 2-copy-of-code/lesson-12/12-todo-list.js
@@ -0,0 +1,51 @@
+let todoList = [
+  {
+    name: "make dinner",
+    dueDate: "29-07-2026",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "29-07-2026",
+  },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate } = todoObject;
+    const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+          todoList.splice(${index}, 1);
+          renderTodoList();
+        " class="delete-todo-button">
+          Delete
+        </button>
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector(".js-content").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElemet = document.querySelector(".js-input");
+  const name = inputElemet.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate,
+  });
+  inputElemet.value = "";
+
+  renderTodoList();
+}
