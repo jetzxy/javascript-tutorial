5f5962eeda109ba9f8a935287002ee85dbc81c7b
John <jetjet9992002@gmail.com>
2026-08-12 17:24:36 +0800

    Added features like save and update the cartItems

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,9 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import {
+  cart,
+  removeFromCart,
+  calculateCartQuantity,
+  updateQuantity,
+} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -31,9 +36,13 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
                 <div class="product-quantity">
-                  <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
-                  <span class="update-quantity-link link-primary">
+                  <span> Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span> </span>
+                  <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
                     Update
+                  </span>
+                  <input type="number" class="quantity-input js-quantity-input-${matchingProduct.id}"">
+                  <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">
+                  Save
                   </span>
                   <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
@@ -83,6 +92,8 @@ cart.forEach((cartItem) => {
             </div>
           </div>
     `;
+
+  updateCartQuantity();
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
@@ -95,5 +106,55 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-container-${productId}`,
     );
     container.remove();
+
+    updateCartQuantity();
   });
 });
+
+document.querySelectorAll(".js-update-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`,
+    );
+    container.classList.add("is-editing-quantity");
+  });
+});
+
+document.querySelectorAll(".js-save-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+
+    const quantityInput = document.querySelector(
+      `.js-quantity-input-${productId}`,
+    );
+
+    const newQuantity = Number(quantityInput.value);
+
+    if (newQuantity < 0 || newQuantity >= 1000) {
+      alert("Quantity must be at least 0 and less than 1000");
+      return;
+    }
+
+    updateQuantity(productId, newQuantity);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`,
+    );
+    container.classList.remove("is-editing-quantity");
+
+    const quantityLabel = document.querySelector(
+      `.js-quantity-label-${productId}`,
+    );
+
+    quantityLabel.innerHTML = newQuantity;
+    updateCartQuantity();
+  });
+});
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector(".js-checkout-items").innerHTML =
+    `${cartQuantity} items`;
+}
